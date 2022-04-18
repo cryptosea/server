db83ef9466d4b8088fe0cf4db128b240502c2692
just-do-halee <just.do.halee@gmail.com>
2022-04-19 00:18:13 +0900

    production to prod

--- src/env/env.module.ts
+++ src/env/env.module.ts
@@ -13,28 +13,31 @@ export class EnvModule {
     list: ['dev', 'test', 'production'],
     strict: true,
   });
-  static readonly config = joinMode(EnvModule.MODE, {
-    /* ********* CAN BE MODIFIED *********** */
-    env: '.env.',
-    DB_HOST_: '',
-    DB_PORT_: '',
-    DB_USERNAME_: '',
-    DB_PASSWORD_: '',
-    DB_NAME_: '',
-    SECRET_KEY_: '',
-    AWS_BUCKET_NAME_: '',
-    AWS_ACCESS_KEY_: '',
-    AWS_SECRET_ACCESS_KEY_: '',
-    WEB3_PROVIDER_URL_: '',
-    WEB3_SECRET_KEY_: '',
-    WEB3_CRYPTOSEA_CONTADDR_: '',
-    WEB3_CRYPTOSEA_CONFIRMATION_: '',
-    WEB3_CRYPTOSEA_GENESISBLOCK_: '',
-    WEB3_CRYPTOSEA_TOPIC_TOKENURI_: '',
-    IPFS_FILE_FETCHING_TIMEOUT_: '',
-    ETHERSCAN_ACCESS_KEY_: '',
-    /* ********* CAN BE MODIFIED *********** */
-  });
+  static readonly config = joinMode(
+    EnvModule.MODE === 'production' ? 'prod' : EnvModule.MODE,
+    {
+      /* ********* CAN BE MODIFIED *********** */
+      env: '.env.',
+      DB_HOST_: '',
+      DB_PORT_: '',
+      DB_USERNAME_: '',
+      DB_PASSWORD_: '',
+      DB_NAME_: '',
+      SECRET_KEY_: '',
+      AWS_BUCKET_NAME_: '',
+      AWS_ACCESS_KEY_: '',
+      AWS_SECRET_ACCESS_KEY_: '',
+      WEB3_PROVIDER_URL_: '',
+      WEB3_SECRET_KEY_: '',
+      WEB3_CRYPTOSEA_CONTADDR_: '',
+      WEB3_CRYPTOSEA_CONFIRMATION_: '',
+      WEB3_CRYPTOSEA_GENESISBLOCK_: '',
+      WEB3_CRYPTOSEA_TOPIC_TOKENURI_: '',
+      IPFS_FILE_FETCHING_TIMEOUT_: '',
+      ETHERSCAN_ACCESS_KEY_: '',
+      /* ********* CAN BE MODIFIED *********** */
+    },
+  );
   // mode-one
   static envs = getEnvs(EnvModule.config, {
     exceptKeys: ['env'],
@@ -46,7 +49,7 @@ export class EnvModule {
     const configModule = ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: config.env,
-      ignoreEnvFile: config.__MODE__ === 'production',
+      ignoreEnvFile: config.__MODE__ === 'prod',
       validationSchema: Joi.object({
         /** TODO */
       }),
